2dc6bec4660d646b83517e691b1485af8411e6a5
aaa <bbb>
2021-09-01 10:01:17 +0800

    Add win32 support

--- demo/demo.c
+++ demo/demo.c
@@ -1,4 +1,4 @@
-//
+﻿//
 // Created by MightyPork on 2017/10/15.
 //
 
@@ -8,29 +8,54 @@
 #define _GNU_SOURCE
 #define __USE_GNU
 
+#ifdef _MSC_VER
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#pragma comment(lib,"Ws2_32")
+#define ssize_t size_t
+#define close closesocket
+#else
 #include <sched.h>
-
-#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <string.h>
 #include <arpa/inet.h>
+#endif
 #include <signal.h>
+#include <string.h>
 #include <malloc.h>
 #include <stdlib.h>
 
 volatile int sockfd = -1;
 volatile bool conn_disband = false;
 
-TinyFrame *demo_tf;
+TinyFrame* demo_tf;
 
+
+
+#ifdef _MSC_VER
+static void usleep(unsigned long usec)
+{
+	HANDLE timer;
+	LARGE_INTEGER interval;
+	interval.QuadPart = -(long)(10 * usec);
+
+	timer = CreateWaitableTimer(NULL, TRUE, NULL);
+	SetWaitableTimer(timer, &interval, 0, NULL, NULL, 0);
+	WaitForSingleObject(timer, INFINITE);
+	CloseHandle(timer);
+}
+#endif
 /**
  * Close socket
  */
 void demo_disconn(void)
 {
-    conn_disband = true;
-    if (sockfd >= 0) close(sockfd);
+#ifdef _MSC_VER
+	WSACleanup();
+#endif // _MSC_VER
+
+	conn_disband = true;
+	if (sockfd >= 0) close(sockfd);
 }
 
 /**
@@ -39,18 +64,18 @@ void demo_disconn(void)
  * @param buff
  * @param len
  */
-void TF_WriteImpl(TinyFrame *tf, const uint8_t *buff, uint32_t len)
+void TF_WriteImpl(TinyFrame* tf, const uint8_t* buff, uint32_t len)
 {
-    printf("\033[32mTF_WriteImpl - sending frame:\033[0m\n");
-    dumpFrame(buff, len);
-    usleep(1000);
+	printf("\033[32mTF_WriteImpl - sending frame:\033[0m\n");
+	dumpFrame(buff, len);
+	usleep(1000);
 
-    if (sockfd != -1) {
-        write(sockfd, buff, len);
-    }
-    else {
-        printf("\nNo peer!\n");
-    }
+	if (sockfd != -1) {
+		send(sockfd, buff, len,0);
+	}
+	else {
+		printf("\nNo peer!\n");
+	}
 }
 
 
@@ -60,46 +85,50 @@ void TF_WriteImpl(TinyFrame *tf, const uint8_t *buff, uint32_t len)
  * @param unused
  * @return unused
  */
-static int demo_client(void *unused)
+#ifdef _MSC_VER
+DWORD WINAPI demo_client(LPVOID unused)
+#else
+static int demo_client(void* unused)
+#endif
 {
-    (void) unused;
+	(void)unused;
 
-    ssize_t n = 0;
-    uint8_t recvBuff[1024];
-    struct sockaddr_in serv_addr;
+	ssize_t n = 0;
+	uint8_t recvBuff[1024];
+	struct sockaddr_in serv_addr;
 
-    printf("\n--- STARTING CLIENT! ---\n");
+	printf("\n--- STARTING CLIENT! ---\n");
 
-    memset(recvBuff, '0', sizeof(recvBuff));
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Error : Could not create socket \n");
-        return false;
-    }
+	memset(recvBuff, '0', sizeof(recvBuff));
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Error : Could not create socket \n");
+		return false;
+	}
 
-    memset(&serv_addr, '0', sizeof(serv_addr));
+	memset(&serv_addr, '0', sizeof(serv_addr));
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("\n inet_pton error occured\n");
-        return false;
-    }
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+		printf("\n inet_pton error occured\n");
+		return false;
+	}
 
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\n Error : Connect Failed \n");
-        perror("PERROR ");
-        return false;
-    }
+	if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+		printf("\n Error : Connect Failed \n");
+		perror("PERROR ");
+		return false;
+	}
 
-    printf("\n Child Process \n");
+	printf("\n Child Process \n");
 
-    while ((n = read(sockfd, recvBuff, sizeof(recvBuff) - 1)) > 0) {
-        printf("\033[36m--- RX %ld bytes ---\033[0m\n", n);
-        dumpFrame(recvBuff, (size_t) n);
-        TF_Accept(demo_tf, recvBuff, (size_t) n);
-    }
-    return 0;
+	while ((n = recv(sockfd, recvBuff, sizeof(recvBuff) - 1, 0)) > 0) {
+		printf("\033[36m--- RX %ld bytes ---\033[0m\n", n);
+		dumpFrame(recvBuff, (size_t)n);
+		TF_Accept(demo_tf, recvBuff, (size_t)n);
+	}
+	return 0;
 }
 
 /**
@@ -108,59 +137,63 @@ static int demo_client(void *unused)
  * @param unused
  * @return unused
  */
-static int demo_server(void *unused)
+#ifdef _MSC_VER
+DWORD WINAPI demo_server(LPVOID unused)
+#else
+static int demo_server(void* unused)
+#endif 
 {
-    (void) unused;
-    ssize_t n;
-    int listenfd = 0;
-    uint8_t recvBuff[1024];
-    struct sockaddr_in serv_addr;
-    int option;
+	(void)unused;
+	ssize_t n;
+	int listenfd = 0;
+	uint8_t recvBuff[1024];
+	struct sockaddr_in serv_addr;
+	int option;
 
-    printf("\n--- STARTING SERVER! ---\n");
+	printf("\n--- STARTING SERVER! ---\n");
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    memset(&serv_addr, '0', sizeof(serv_addr));
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&serv_addr, '0', sizeof(serv_addr));
 
-    option = 1;
-    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (char *) &option, sizeof(option));
+	option = 1;
+	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (char*)&option, sizeof(option));
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(PORT);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(PORT);
 
-    if (bind(listenfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Failed to bind");
-        return 1;
-    }
+	if (bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+		perror("Failed to bind");
+		return 1;
+	}
 
-    if (listen(listenfd, 10) < 0) {
-        perror("Failed to listen");
-        return 1;
-    }
+	if (listen(listenfd, 10) < 0) {
+		perror("Failed to listen");
+		return 1;
+	}
 
-    while (1) {
-        printf("\nWaiting for client...\n");
-        sockfd = accept(listenfd, (struct sockaddr *) NULL, NULL);
-        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *) &option, sizeof(option));
-        printf("\nClient connected\n");
-        conn_disband = false;
+	while (1) {
+		printf("\nWaiting for client...\n");
+		sockfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+		setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char*)&option, sizeof(option));
+		printf("\nClient connected\n");
+		conn_disband = false;
 
-        while ((n = read(sockfd, recvBuff, sizeof(recvBuff) - 1)) > 0 && !conn_disband) {
-            printf("\033[36m--- RX %ld bytes ---\033[0m\n", n);
-            dumpFrame(recvBuff, n);
-            TF_Accept(demo_tf, recvBuff, (size_t) n);
-        }
+		while ((n = recv(sockfd, recvBuff, sizeof(recvBuff) - 1,0)) > 0 && !conn_disband) {
+			printf("\033[36m--- RX %ld bytes ---\033[0m\n", n);
+			dumpFrame(recvBuff, n);
+			TF_Accept(demo_tf, recvBuff, (size_t)n);
+		}
 
-        if (n < 0) {
-            printf("\n Read error \n");
-        }
+		if (n < 0) {
+			printf("\n Read error \n");
+		}
 
-        printf("Closing socket\n");
-        close(sockfd);
-        sockfd = -1;
-    }
-    return 0;
+		printf("Closing socket\n");
+		close(sockfd);
+		sockfd = -1;
+	}
+	return 0;
 }
 
 /**
@@ -170,11 +203,11 @@ static int demo_server(void *unused)
  */
 static void signal_handler(int sig)
 {
-    (void) sig;
-    printf("Shutting down...");
-    demo_disconn();
+	(void)sig;
+	printf("Shutting down...");
+	demo_disconn();
 
-    exit(sig); // pass the signal through - this is nonstandard behavior but useful for debugging
+	exit(sig); // pass the signal through - this is nonstandard behavior but useful for debugging
 }
 
 /**
@@ -182,7 +215,7 @@ static void signal_handler(int sig)
  */
 void demo_sleep(void)
 {
-    while (1) usleep(10);
+	while (1) usleep(10);
 }
 
 /**
@@ -194,33 +227,52 @@ void demo_sleep(void)
  */
 void demo_init(TF_Peer peer)
 {
-    signal(SIGTERM, signal_handler);
-    signal(SIGINT, signal_handler);
+	int retc;
 
-    int retc;
-    void *stack = malloc(8192);
-    if (stack == NULL) {
-        perror("Oh fuck");
-        signal_handler(9);
-        return;
-    }
+	signal(SIGTERM, signal_handler);
+	signal(SIGINT, signal_handler);
 
-    printf("Starting %s...\n", peer == TF_MASTER ? "MASTER" : "SLAVE");
+	printf("Starting %s...\n", peer == TF_MASTER ? "MASTER" : "SLAVE");
+#ifdef _MSC_VER
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    // CLONE_VM    --- share heap
-    // CLONE_FILES --- share stdout and stderr
-    if (peer == TF_MASTER) {
-        retc = clone(&demo_client, (char *) stack + 8192, CLONE_VM | CLONE_FILES, 0);
-    }
-    else {
-        retc = clone(&demo_server, (char *) stack + 8192, CLONE_VM | CLONE_FILES, 0);
-    }
+	HANDLE hThread;
+	DWORD  threadId;
+	if (peer == TF_MASTER) {
+		retc = CreateThread(NULL, 0, demo_client, 0, 0, &threadId);
+	}
+	else {
+		retc = CreateThread(NULL, 0, demo_server, 0, 0, &threadId);
+	}
 
-    if (retc == 0) {
-        perror("Clone fail");
-        signal_handler(9);
-        return;
-    }
+	if (retc == NULL) {
+		perror("Creating new thread fail");
+		signal_handler(9);
+		return;
+	}
+#else
+	void* stack = malloc(8192);
+	if (stack == NULL) {
+		perror("Oh fuck");
+		signal_handler(9);
+		return;
+	}
+	// CLONE_VM    --- share heap
+// CLONE_FILES --- share stdout and stderr
+	if (peer == TF_MASTER) {
+		retc = clone(&demo_client, (char*)stack + 8192, CLONE_VM | CLONE_FILES, 0);
+	}
+	else {
+		retc = clone(&demo_server, (char*)stack + 8192, CLONE_VM | CLONE_FILES, 0);
+	}
 
-    printf("Thread started\n");
+	if (retc == 0) {
+		perror("Clone fail");
+		signal_handler(9);
+		return;
+	}
+#endif
+
+	printf("Thread started\n");
 }
